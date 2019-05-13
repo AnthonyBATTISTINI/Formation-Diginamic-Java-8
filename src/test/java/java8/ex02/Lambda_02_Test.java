80aeab94b413e00551232b1cab2b8095c01210a9
Battistini <battistini.anthony@gmail.com>
2019-05-13 23:54:18 +0200

    02-lambda

---
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -0,0 +1,64 @@
+package java8.ex02;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import java8.data.Account;
+import java8.data.Data;
+import java8.data.Person;
+
+/**
+ * Exercice 02 - Map
+ */
+public class Lambda_02_Test {
+
+	// tag::PersonToAccountMapper[]
+    interface PersonToAccountMapper {
+        Account map(Person p);
+    }
+    // end::PersonToAccountMapper[]
+
+    // tag::map[]
+    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
+        // TODO implémenter la méthode pour transformer une liste de personnes en liste de comptes
+        return null;
+    }
+    // end::map[]
+
+
+    // tag::test_map_person_to_account[]
+    @Test
+    public void test_map_person_to_account() throws Exception {
+
+        List<Person> personList = Data.buildPersonList(100);
+
+        // TODO transformer la liste de personnes en liste de comptes
+        // TODO tous les objets comptes ont un solde à 100 par défaut
+        List<Account> result = map(personList, null);
+
+        assert result.size() == personList.size();
+        for (Account account : result) {
+            assert account.getBalance().equals(100);
+            assert account.getOwner() != null;
+        }
+    }
+    // end::test_map_person_to_account[]
+
+    // tag::test_map_person_to_firstname[]
+    @Test
+    public void test_map_person_to_firstname() throws Exception {
+
+        List<Person> personList = Data.buildPersonList(100);
+
+        // TODO transformer la liste de personnes en liste de prénoms
+        List<String> result = null;
+
+        assert result.size() == personList.size();
+        for (String firstname : result) {
+            assert firstname.startsWith("first");
+        }
+    }
+    // end::test_map_person_to_firstname[]
+	
+}
---
+++ src/test/java/java8/ex03/Lambda_03_Test.java
@@ -0,0 +1,46 @@
+package java8.ex03;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import java8.data.Data;
+import java8.data.Person;
+
+/**
+ * Exercice 03 - ForEach
+ */
+public class Lambda_03_Test {
+
+	// tag::PersonProcessor[]
+    interface PersonProcessor {
+        void process(Person p);
+    }
+    // end::PersonProcessor[]
+
+    // tag::forEach[]
+    private void forEach(List<Person> source, PersonProcessor processor) {
+       // TOD0
+    }
+    // end::forEach[]
+
+
+    // tag::test_verify_person[]
+    @Test
+    public void test_verify_person() throws Exception {
+
+        List<Person> personList = Data.buildPersonList(100);
+
+        // TODO vérifier qu'une personne à un prénom qui commence par first
+        // TODO vérifier qu'une personne à un nom qui commence par last
+        // TODO vérifier qu'une personne à un age > 0
+        // TODO la vérification se fait via une assertion (mot clé assert)
+        PersonProcessor verifyPerson = null;
+
+        assert verifyPerson != null;
+
+        forEach(personList, verifyPerson);
+    }
+    // end::test_verify_person[]
+	
+}
