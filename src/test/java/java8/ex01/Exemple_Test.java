af39586a90e01d1eb3a0b5f3be6e2bb51ed3d158
Battistini <battistini.anthony@gmail.com>
2019-05-15 19:33:32 +0200

    00-exemple

---
+++ src/test/java/java8/ex01/Exemple_Test.java
@@ -0,0 +1,17 @@
+package java8.ex01;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Exemple Test
+ */
+public class Exemple_Test {
+
+	@Test
+	public void testQuiEchoue() {
+		fail();
+	}
+	
+}
